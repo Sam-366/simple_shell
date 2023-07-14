8c0be93ea7f24328d0ccc974c11b77b3c2ae9967
Itumeleng Malgas <aaron.tumi@live.co.za>
2023-07-14 19:11:00 +0200

    Prog Entry

---
+++ prog_entry.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	/* Non-interactive mode */
+	if (argc > 1)
+	{
+
+	}
+	/* Interactive mode */
+	else
+	{
+
+	}
+
+	return (0);
+}
