01eb0d16999fdb7cac1e13595b206f64d9d7be71
Sam-366 <ntlombe.s@gmail.com>
2023-07-23 13:32:34 -0700

    paramter fixed

--- prompt.c
+++ prompt.c
@@ -1,26 +1,83 @@
 #include "shell.h"
 
+#define MAX_INPUT_LENGTH 1024
 #define PROMPT "$ "
-#define MAX_INPUT_LENGTH 100
 
-int main() 
+void execute_command(char **args) 
 {
-       	char input[MAX_INPUT_LENGTH];
-       
+       	pid_t pid = fork();
+
+	if (pid < 0) 
+	{
+		perror("Fork failed");
+	} 
+	else if (pid == 0) 
+	{
+		if (execvp(args[0], args) == -1) 
+		{
+			perror("Execution failed");
+			exit(EXIT_FAILURE);
+		}
+		exit(EXIT_SUCCESS);
+	} 
+	else 
+	{
+        int status;
+        waitpid(pid, &status, 0);
+	}
+}
+int main(int argc, char *argv[]) 
+{
+	char input[MAX_INPUT_LENGTH];
+	char *args[MAX_INPUT_LENGTH / 2 + 1];
+	
 	while (1) 
 	{
-		printf(PROMPT);
-		
-		if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
-		{
-			break;
-		}
-		size_t len = strlen(input);
-		if (len > 0 && input[len - 1] == '\n') 
-		{
-			input[len - 1] = '\0';
-		}
-		printf("Command entered: %s\n", input);
+		printf("myshell> ");
+		fflush(stdout);
+
+        
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL) 
+	{
+            
+            break;
+        }
+
+        /* Tokenize the input */
+
+        char *token = strtok(input, " \t\n");
+        int arg_index = 0;
+        while (token != NULL) 
+	{
+		args[arg_index++] = token;
+		token = strtok(NULL, " \t\n");
 	}
-	return 0;
+        args[arg_index] = NULL; 
+	
+	if (arg_index > 0) 
+	{
+		if (strcmp(args[0], "exit") == 0) 
+		{
+                break;
+		} 
+		else if (strcmp(args[0], "cd") == 0) 
+		{
+			if (arg_index > 1) 
+			{
+				if (chdir(args[1]) != 0) 
+				{
+					perror("cd failed");
+				}
+			} 
+			else 
+			{
+                }
+                continue;
+            }
+        }
+        execute_command(args);
+    }
+
+    return 0;
 }
+
